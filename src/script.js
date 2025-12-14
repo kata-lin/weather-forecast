81ecdb9870236aa63e17665ec4f026768d895400
Kata <katabalogh@outlook.com>
2025-12-14 16:14:09 +0000

    inject more weather data

--- src/script.js
+++ src/script.js
@@ -1,11 +1,33 @@
 function updateWeatherData(response) {
+    let cityElement = document.querySelector("#city");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time *1000);
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
     let temperatureElement = document.querySelector("#weather-temperature");
     let currentTemperature = Math.round(response.data.temperature.current);
-    let cityElement = document.querySelector("#city");
     
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = currentTemperature;
 };
+
+function formatDate(date) {
+    let days = ["Sunday", "Monday", "Tuesdya", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`
+}
 
 function searchCity(city) {
     let apiKey = "t9425412e35f60bab5f5aa66fa3o377c";
